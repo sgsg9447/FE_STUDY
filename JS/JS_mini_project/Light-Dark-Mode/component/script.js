ac436e6e6a6275e48b73cff4f86035c9690e1b02
seulgikim <sgsg9447@gmail.com>
2022-06-15 13:48:48 +0900

    다크모드 컴포넌트화 구현현

--- JS/JS_mini_project/Light-Dark-Mode/component/script.js
+++ JS/JS_mini_project/Light-Dark-Mode/component/script.js
@@ -1,41 +1,24 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById("nav");
 const toggleIcon = document.getElementById("toggle-icon");
-const image1 = document.getElementById("image1");
-const image2 = document.getElementById("image2");
-const image3 = document.getElementById("image3");
-const textBox = document.getElementById("text-box");
 
-// Dark or Light Images
-function imageMode(color) {
-  image1.src = `img/undraw_proud_coder_${color}.svg`;
-  image2.src = `img/undraw_feeling_proud_${color}.svg`;
-  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-}
-
-//Dark Mode Styles
+// DarkMode Style
 function darkMode() {
-  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
   toggleIcon.children[0].textContent = "Dark Mode";
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  imageMode("dark");
 }
 
-//Dark Mode Styles
+// LightMode Style
 function lightMode() {
-  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
   toggleIcon.children[0].textContent = "Light Mode";
   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  imageMode("light");
 }
 
-//Switch Theme Dynamically
-function switchTheme(event) {
-  if (event.target.checked) {
+// Swithch Theme Dynamically
+function switchTheme(e) {
+  if (e.target.checked) {
     //documentElement 문서의 루트인 요소를 반환함 따라서 해당 요소에 다크테마속성 설정함
-    document.documentElement.setAttribute("data-theme", "dark"); /*키,밸류*/
+    document.documentElement.setAttribute("data-theme", "dark"); /*키, 밸류 */
     localStorage.setItem("theme", "dark");
     darkMode();
   } else {
@@ -51,7 +34,10 @@ toggleSwitch.addEventListener("change", switchTheme);
 // Check Local Storage For Theme
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme); /*키,밸류*/
+  document.documentElement.setAttribute(
+    "data-theme",
+    currentTheme
+  ); /* 키, 밸류 */
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
     darkMode();
