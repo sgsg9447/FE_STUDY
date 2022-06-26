7430f304aa649c16dbb28681d58aabd3c023c7b1
sgsg9447 <sgsg9447@gmail.com>
2022-06-26 13:53:01 +0900

    리액트 simple_diary 시작 및 첫페이지 입력폼 완성

---
+++ React/simple_diary/src/DiaryEditor.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChaneState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장성공");
+  };
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input name="author" value={state.author} onChange={handleChaneState} />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChaneState}
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChaneState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
