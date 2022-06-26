0fc3472acc7643fdec85549d8b3550d4e2aaeb90
sgsg9447 <sgsg9447@gmail.com>
2022-06-26 14:30:01 +0900

    React DailyDiary 첫화면 기능 구현

---
+++ React/dailydiary/src/DiaryEditor.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    console.log(state);
+    alert("저장성공");
+  };
+  return (
+    // className 을 파일명과 동일하게 해준 이유는 css 조작 쉽게하기위해!
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+export default DiaryEditor;
