c97625f764c5066551f1afe31c296e913fd76ec2
sgsg9447 <sgsg9447@gmail.com>
2022-06-27 19:57:40 +0900

    로그인 로컬스토리지 기능 구현

---
+++ React/login-page/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginLocalStorage from "./LoginLocalStorage";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginLocalStorage />
+    </div>
+  );
+}
+
+export default App;
